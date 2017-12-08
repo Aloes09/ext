c82bf3e9b7d61f5b5be48164ed858157fbfd355d
Aloes09 <arkadiuszsolarz.as@gmail.com>
2017-12-08 10:34:26 +0100

    drobne poprawki

--- popup.js
+++ popup.js
@@ -4,12 +4,13 @@ if (localStorage.getItem('isStreamOnline') === 'false')
 {
     stat.innerHTML = "OFFLINE";
     stat.style.color = "red";
+    document.getElementById("game").style.fontSize="0px";
 
 } else
 {
     stat.innerHTML = "ONLINE";
     stat.style.color = "#00b200";
-    if (localStorage.getItem("game") != null)
+    if (localStorage.getItem("game") !== null)
     {
         document.getElementById("game").innerHTML = "W grze: " + localStorage.getItem("game");
     }
